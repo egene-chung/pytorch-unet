fe0117f19661635a5f758c6982956bd1dc877616
egene-chung <egenechung@gmail.com>
2024-07-18 02:17:44 +0900

    Add: UNet train val 추가

--- train.py
+++ train.py
@@ -91,7 +91,7 @@ class UNet(nn.Module):
         self.decoder1_2 = CBR2d(in_channels=2 * 64, out_channels=64)
         self.decoder1_1 = CBR2d(in_channels=64, out_channels=64)
         
-        self.fc = nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1, stride=1, padding=0, bias=True)
+        self.fc = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0, bias=True)
 
     # unet layer 연결하기
     # forward func -> init 에서 생성한 layer를 연결하는 코드 작성, x: input img
@@ -265,7 +265,118 @@ label = data['label']
 print(label.shape) # torch.Size([1, 512, 512])
 print(label.type()) # torch.FloatTensor
  
+# training 및 validation framework 구현
+transform = transforms.Compose([Normalization(mean=0.5, std=0.5), RandomFlip(), ToTensor()])
 
+dataset_train = Dataset(data_dir=os.path.join(data_dir, 'train'), transform=transform)
+train_loader = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=8)
 
+dataset_val = Dataset(data_dir=os.path.join(data_dir, 'val'), transform=transform)
+val_loader = DataLoader(dataset_val, batch_size=batch_size, shuffle=True, num_workers=8)
 
+# 네트워크 생성하기
+# 네트워크가 학습되는 도메인이 cpu인지 gpu인지 명시해주기 위해 .to(device)
+model = UNet().to(device)
 
+# 손실 함수 정의하기
+fn_loss = nn.BCEWithLogitsLoss().to(device)
+
+# Optimizer 설정하기
+optim = torch.optim.Adam(model.parameters(), lr = lr)
+
+# loss를 계산할 때 필요한 여러가지 variables 설정하기
+num_data_train = len(dataset_train)
+num_data_val = len(dataset_val)
+
+# batch size 에 의해 나누어지는 dataset 수
+num_batch_train = np.ceil(num_data_train / batch_size)
+num_batch_val = np.ceil(num_data_val / batch_size)
+
+# output을 저장하기 위해 필요한 몇 가지 부수적인 functions 설정하기
+# tensor variable에서 numpy로 transfer시키는 함수
+fn_tonumpy = lambda x: x.to('cpu').detach().numpy().transpose(0, 2, 3, 1)
+fn_denorm = lambda x, mean, std: (x * std) + mean
+fn_class = lambda x: 1.0 * ( x > 0.5)
+
+# Tensorboard를 사용하기 위한 SummaryWriter 설정
+writer_train = SummaryWriter(log_dir=os.path.join(log_dir, 'train'))
+writer_val = SummaryWriter(log_dir=os.path.join(log_dir, 'val'))
+
+# 네트워크 학습시키기: training이 진행되는 for loop 구현
+st_epoch = 0
+
+for epoch in range(st_epoch + 1, num_epoch + 1):
+    # 네트워크에게 training 모드임을 알려주는 training 모드 activate 
+    model.train()
+    loss_arr = []
+
+    for batch, data in enumerate(train_loader, 1):
+        label = data['label'].to(device)
+        input = data['input'].to(device)
+
+        output = model(input)
+
+        # backward pass : backward propagation
+        optim.zero_grad()
+
+        loss = fn_loss(output, label)
+        loss.backward()
+
+        optim.step()
+
+        # 손실함수 계산
+        loss_arr += [loss.item()]
+
+        print("TRAIN: EPOCH %04d / %04d | BATCH %04d / %04d | LOSS %.4f" %
+              (epoch, num_epoch, batch, num_batch_train, np.mean(loss_arr)))
+
+        # Tensorboard 저장하기
+        label = fn_tonumpy(label)
+        input = fn_tonumpy(fn_denorm(input, mean=0.5, std=0.5))
+        output = fn_tonumpy(fn_class(output))
+
+        # label, input, output 이미지를 텐서보드에 저장
+        writer_train.add_image('label', label, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+        writer_train.add_image('input', input, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+        writer_train.add_image('output', output, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+
+    # loss를 텐서보드에 저장하는 부분
+    writer_train.add_scalar('loss', np.mean(loss_arr), epoch)
+
+# 네트워크 validation - back propagation 없어.
+# back propagation을 사전에 막기 위해 torch.no_grad()사용
+
+    with torch.no_grad():
+        model.eval()
+        loss_arr = []
+
+        for batch, data in enumerate(val_loader, 1):
+            # forward pass
+            label = data['label'].to(device)
+            input = data['input'].to(device)
+
+            output = model(input)
+
+            # backward 없어
+
+            # 손실함수 계한
+            loss = fn_loss(output, label)
+
+            loss_arr += [loss.item()]
+
+            print("TRAIN: EPOCH %04d / %04d | BATCH %04d / %04d | LOSS %.4f" %
+              (epoch, num_epoch, batch, num_batch_val, np.mean(loss_arr)))
+            
+            # Tensorboard에 output 저장하기
+            label = fn_tonumpy(label)
+            input = fn_tonumpy(fn_denorm(input, mean=0.5, std=0.5))
+            output = fn_tonumpy(fn_class(output))
+
+            writer_val.add_image('label', label, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
+            writer_val.add_image('input', input, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
+            writer_val.add_image('output', output, num_batch_val * (epoch - 1) + batch, dataformats='NHWC')
+        
+    writer_val.add_scalar('loss', np.mean(loss_arr), epoch)
+
+writer_train.close()
+writer_val.close()
